c294c9c15441e3af7df0135b08b3f9bef4f50637
Viswa1103 <viswa.vl1103@gmail.com>
2024-06-14 18:00:28 +0530

    somebasic

---
+++ SimplePrograms/CountingOccurance.java
@@ -0,0 +1,23 @@
+package SimplePrograms;
+
+import java.util.Scanner;
+
+public class CountingOccurance {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Enter the number to check");
+        int c = sc.nextInt();
+        int count = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem == c) {
+                count++;
+            }
+            n = n / 10;
+
+        }
+        System.out.println(count);
+    }
+}
---
+++ SimplePrograms/fibo.java
@@ -0,0 +1,24 @@
+package SimplePrograms;
+
+import java.util.Scanner;
+
+public class fibo {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 2;
+        while (count <= n) {
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+            System.out.print(b + " ");
+
+        }
+        // System.out.println(b);
+
+    }
+
+}
