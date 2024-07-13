981c6daf600094a5e48cd35d8f2d2f50e8417678
Viswa1103 <viswa.vl1103@gmail.com>
2024-07-13 13:31:54 +0530

    N-Queen problem

---
+++ MazeProblem/NQueen.java
@@ -0,0 +1,67 @@
+package MazeProblem;
+
+public class NQueen {
+    public static void main(String[] args) {
+
+        int n = 4;
+        boolean[][] board = new boolean[n][n];
+        System.out.println(queen(board, 0));
+
+    }
+
+    static int queen(boolean[][] board, int r) {
+        if (r == board.length) {
+            display(board);
+            System.out.println();
+
+            return 1;
+        }
+        int count = 0;
+
+        for (int c = 0; c < board.length; c++) {
+            if (isSafe(board, r, c)) {
+                board[r][c] = true;
+                count = count + queen(board, r + 1);
+                board[r][c] = false;
+
+            }
+        }
+        return count;
+    }
+
+    private static boolean isSafe(boolean[][] board, int r, int c) {
+        for (int i = 0; i < r; i++) {
+            if (board[i][c]) {
+                return false;
+            }
+        }
+
+        int maxl = Math.min(r, c);
+        for (int i = 1; i <= maxl; i++) {
+            if (board[r - i][c - i]) {
+                return false;
+            }
+        }
+        int maxr = Math.min(r, board.length - c - 1);
+        for (int i = 1; i <= maxr; i++) {
+            if (board[r - i][c + i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void display(boolean[][] board) {
+        for (boolean[] row : board) {
+            for (boolean ele : row) {
+                if (ele) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
