0d8f5b2464ff28db8c23af2259620073682b2110
Viswa1103 <viswa.vl1103@gmail.com>
2024-07-20 23:49:11 +0530

    circular linked list done

---
+++ LinkedList/Circular/CLL.java
@@ -0,0 +1,52 @@
+package LinkedList.Circular;
+
+import SimplePrograms.prime;
+
+public class CLL {
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public void insertFirst(int value) {
+        Node node = new Node(value);
+        if (head == null) {
+            head = node;
+            tail = node;
+            return;
+        }
+
+        tail.next = node;
+        node.next = head;
+        tail = node;
+        size++;
+
+    }
+
+    public void display() {
+        Node node = head;
+        if (head != null) {
+            do {
+                System.out.print(node.value + "-->");
+                node = node.next;
+            } while (node != head);
+            System.out.println("END");
+        }
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+}
---
+++ LinkedList/Circular/Main.java
@@ -0,0 +1,13 @@
+package LinkedList.Circular;
+
+public class Main {
+    public static void main(String[] args) {
+        CLL list = new CLL();
+        list.insertFirst(33);
+        list.insertFirst(43);
+        list.insertFirst(53);
+        list.insertFirst(63);
+        list.display();
+
+    }
+}
