a37c9a9ad0fe07b2a269006c3b22149de9d0d903
Viswa1103 <viswa.vl1103@gmail.com>
2024-08-03 20:19:43 +0530

    BST implmentation

---
+++ Tree/BST/Main.java
@@ -0,0 +1,16 @@
+package Tree.BST;
+
+public class Main {
+    public static void main(String[] args) {
+        BST tree = new BST();
+
+        tree.insert(35);
+        tree.insert(55);
+        tree.insert(37);
+        tree.insert(25);
+        tree.insert(78);
+        tree.insert(100);
+        tree.display();
+
+    }
+}
